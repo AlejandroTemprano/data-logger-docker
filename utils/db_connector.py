f9b167ef5000a44b26c5daf566fc27f04a011ccf
AlejandroTemprano <65078699+AlejandroTemprano@users.noreply.github.com>
2023-05-25 13:07:33 -0500

    Added send_query and insert_candle (with COPY)

--- utils/db_connector.py
+++ utils/db_connector.py
@@ -1,3 +1,6 @@
+import os
+
+import pandas as pd
 import psycopg
 
 from utils.logger import setup_logger
@@ -43,3 +46,89 @@ class DatabaseConnector:
             return ""
 
         return status
+
+    def send_query(self, sql: str, values: tuple = ()) -> str:
+        """Sends sql query to the db and return all data queried."""
+        try:
+            with psycopg.connect(self.conninfo_str) as conn:
+                with conn.cursor() as cur:
+                    cur.execute(sql, values)
+                    data = cur.fetchall()
+                    conn.commit()
+
+        except psycopg.Error as e:
+            self.logger.error(e)
+            self.logger.error("Unable to execute command.")
+            return ""
+
+        return data
+
+    def insert_candles(self, table_name: str, candle_data: pd.DataFrame):
+        # Saves candle_data on a temp csv file.
+        try:
+            temp_file = f"./temp_{table_name}.csv"
+            candle_data.to_csv(temp_file, index=False, header=False)
+        except Exception as e:
+            self.logger.error(e)
+            self.logger.error("Unable to save data as temp csv.")
+            return
+
+        # Reads the temp csv file and inserts the data into the table_name
+        try:
+            with psycopg.connect(self.conninfo_str) as conn:
+                with conn.cursor() as cur:
+                    # Get the table columns from the table
+                    cur.execute(
+                        f"""SELECT column_name
+                        FROM information_schema.columns
+                        WHERE table_schema = 'public'
+                        AND table_name   = '{table_name}'
+                        ORDER BY ordinal_position;"""
+                    )
+                    table_columns = cur.fetchall()
+                    table_columns.pop(0)
+                    table_columns = [x[0] for x in table_columns]
+                    table_columns_str = ", ".join(table_columns)
+
+                    # Create a staging table
+                    cur.execute(
+                        f"""CREATE TEMPORARY TABLE staging_table_{table_name}
+                        (LIKE {table_name}
+                        INCLUDING defaults
+                        INCLUDING constraints
+                        INCLUDING indexes);"""
+                    )
+                    if cur.statusmessage != "CREATE TABLE":
+                        self.logger.error(cur.statusmessage)
+                        self.logger.error("Unable to create staging table.")
+                        return
+
+                    # Copy the data from the csv to the staging table
+                    with open(temp_file, "r") as f:
+                        with cur.copy(
+                            f"""COPY staging_table_{table_name} ({table_columns_str}) FROM STDIN WITH (FORMAT CSV);
+                            """
+                        ) as copy:
+                            while data := f.read(8192):
+                                copy.write(data)
+
+                        print(cur.statusmessage)
+
+                    # Insert data from staging table to table_name
+                    do_update_columns_str = ", ".join([f"{col} = excluded.{col}" for col in table_columns])
+                    cur.execute(
+                        f"""INSERT INTO {table_name} ({table_columns_str})
+                        SELECT {table_columns_str}
+                        FROM staging_table_{table_name}
+                        ON CONFLICT (date, market)
+                        DO UPDATE SET {do_update_columns_str};
+                        """
+                    )
+                    self.logger.debug(cur.statusmessage)
+
+                    # Delete temp csv file
+                    os.remove(f"./temp_{table_name}.csv")
+
+        except psycopg.Error as e:
+            self.logger.error(e)
+            self.logger.error("Unable to execute command.")
