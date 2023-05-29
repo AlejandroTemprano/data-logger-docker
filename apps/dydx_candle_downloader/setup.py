114d3d45a3a5953a52b412e21ea90f073b0f8d29
AlejandroTemprano <65078699+AlejandroTemprano@users.noreply.github.com>
2023-05-29 11:10:45 -0500

    Generalize TABLE_NAME and import online markets

--- apps/dydx_candle_downloader/setup.py
+++ apps/dydx_candle_downloader/setup.py
@@ -13,25 +13,27 @@ from datetime import datetime, timezone
 import pandas as pd
 
 from credentials.db_credentials import db_credentials as DB_CREDENTIALS
-from data.config import DYDX_MARKET_LIST_ALL as MARKET_LIST
 from utils.db_connector import DatabaseConnector
 from utils.dydx_client import DydxClient
 from utils.logger import setup_logger
 
 IMPORT_HISTORICAL_CANDLES = True
-START_DATE = datetime(2020, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
-END_DATE = None  # If none, it will use current date
+EXCHANGE_START_DATE = datetime(2020, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+
+TABLE_NAME = "dydx_candles"
 
 
 def setup(logger=None):
     logger = logger if logger else setup_logger(name="db_client_logger", debug_level="DEBUG")
     logger.info("Starting setup for dydx_candle.py")
 
+    # Initialize clients
     db_client = DatabaseConnector(DB_CREDENTIALS, logger)
+    exchange_client = DydxClient(logger)
 
     # Create candles table
-    sql = """
-        CREATE TABLE IF NOT EXISTS dydx_candles (
+    sql = f"""
+        CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
             id BIGSERIAL PRIMARY KEY,
             date TIMESTAMP WITH TIME ZONE,
             updated TIMESTAMP WITH TIME ZONE,
@@ -51,10 +53,10 @@ def setup(logger=None):
         return
     logger.info("Candle table created/already exist.")
 
-    # Set up indexes for candle table
-    sql = """
-        CREATE UNIQUE INDEX IF NOT EXISTS candle_idx_date_market 
-            ON dydx_candles (date, market);
+    # Set up indexes for candles table
+    sql = f"""
+        CREATE UNIQUE INDEX IF NOT EXISTS {TABLE_NAME}_idx_date_market 
+            ON {TABLE_NAME} (date, market);
     """
     msg = db_client.send_request(sql)
     if msg != "CREATE INDEX":
@@ -63,32 +65,48 @@ def setup(logger=None):
         return
     logger.info("Candle index created/already exist.")
 
-    # Import historical candle data from exchange and fill the table
-    if IMPORT_HISTORICAL_CANDLES:
-        debug_time = datetime.now()
-        # Calculate end_date if None
-        end_date = (
-            datetime.utcnow().replace(minute=0, second=0, microsecond=0, tzinfo=timezone.utc)
-            if END_DATE is None
-            else END_DATE
-        )
+    # Checks for previous data inside the table
+    sql = f"""
+        SELECT market, MIN(date) as first_candle, MAX(date) as last_candle
+        FROM {TABLE_NAME}
+        GROUP BY market;
+    """
+    data = db_client.send_query(sql)
+    data = {
+        market: {"first_candle": first_candle, "last_candle": last_candle} for market, first_candle, last_candle in data
+    }
 
-        logger.info(f"Downloading candle date between {START_DATE} and {end_date}...")
+    # Download list with all online markets
+    markets_list = exchange_client.get_online_markets()
 
-        dydx_client = DydxClient(logger)
+    # If previous data is present, it will confirm it has all available data points
+    if bool(data):
+        logger.info("Previous data found.")
+        logger.info(data)
 
-        # Downloads the historical candles for each market
-        download_time = datetime.now()
-        candle_data = dydx_client.get_all_markets_candles(MARKET_LIST, START_DATE, end_date)
-        logger.debug(f"Data downloaded in {datetime.now() - download_time}")
-        logger.info("All candle data downloaded.")
+    # If no previous data is present, it will download it from the exchange
+    else:
+        logger.info("No previous data found, downloading all available candle data from the exchange.")
+        # Import historical candle data from exchange and fill the table
+        if IMPORT_HISTORICAL_CANDLES:
+            debug_time = datetime.now()
 
-        logger.info(f"Saving candle date into dydx_candle table...")
-        insert_time = datetime.now()
-        db_client.insert_candles(table_name="dydx_candles", candle_data=candle_data)
+            end_date = datetime.utcnow().replace(minute=0, second=0, microsecond=0, tzinfo=timezone.utc)
 
-        logger.debug(f"Save the downloaded data to the db took {datetime.now() - insert_time}.")
-        logger.debug(f"Total time to download and save candle data to the db: {datetime.now() - debug_time}.")
+            logger.info(f"Downloading candle date between {EXCHANGE_START_DATE} and {end_date}...")
+
+            # Downloads the historical candles for each market
+            download_time = datetime.now()
+            candle_data = exchange_client.get_all_markets_candles(markets_list, EXCHANGE_START_DATE, end_date)
+            logger.debug(f"Data downloaded in {datetime.now() - download_time}")
+            logger.info("All candle data downloaded.")
+
+            logger.info(f"Saving candle date into dydx_candle table...")
+            insert_time = datetime.now()
+            db_client.insert_candles(table_name="dydx_candles", candle_data=candle_data)
+
+            logger.debug(f"Save the downloaded data to the db took {datetime.now() - insert_time}.")
+            logger.debug(f"Total time to download and save candle data to the db: {datetime.now() - debug_time}.")
 
     logger.info("Setup complete.")
     return
